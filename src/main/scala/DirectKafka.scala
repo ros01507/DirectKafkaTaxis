300d9ecdaa472b2d63cdbbab8c7a0fcce3569535
ros01507 <alfrealcala@gmail.com>
2017-06-16 20:36:51 +0200

    X

--- src/main/scala/DirectKafka.scala
+++ src/main/scala/DirectKafka.scala
@@ -1,23 +1,13 @@
-
 import kafka.serializer.StringDecoder
 import org.apache.spark.SparkConf
-import org.apache.spark.streaming._
-import org.apache.spark.streaming.kafka._
-/**
-  * Consumes messages from one or more topics in Kafka and does wordcount.
-  * Usage: DirectKafkaWordCount <brokers> <topics>
-  *   <brokers> is a list of one or more Kafka brokers
-  *   <topics> is a list of one or more kafka topics to consume from
-  *
-  * Example:
-  *    $ bin/run-example streaming.DirectKafkaWordCount broker1-host:port,broker2-host:port \
-  *    topic1,topic2
-  */
+import org.apache.spark.streaming.kafka.KafkaUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
 object DirectKafkaTaxisNY {
   def main(args: Array[String]) {
     if (args.length < 2) {
       System.err.println(s"""
-                            |Usage: DirectKafkaWordCount <brokers> <topics>
+                            |Usage: DirectKafkaTaxisNY <brokers> <topics>
                             |  <brokers> is a list of one or more Kafka brokers
                             |  <topics> is a list of one or more kafka topics to consume from
                             |
@@ -25,28 +15,24 @@ object DirectKafkaTaxisNY {
       System.exit(1)
     }
 
-   // StreamingExamples.setStreamingLogLevels()
+  //  StreamingExamples.setStreamingLogLevels()
 
     val Array(brokers, topics) = args
 
     // Create context with 2 second batch interval
-    val sparkConf = new SparkConf().setAppName("DirectKafkaTaxisNY").setMaster("local[2]")
+    val sparkConf = new SparkConf().setAppName("DirectKafkaTaxisNY")
     val ssc = new StreamingContext(sparkConf, Seconds(2))
 
     // Create direct kafka stream with brokers and topics
     val topicsSet = topics.split(",").toSet
-    val kafkaParams = Map[String, String](
-      "metadata.broker.list" -> brokers,
-      "group-id" -> "test-consumer-group")
-
+    val kafkaParams = Map[String, String]("metadata.broker.list" -> brokers)
     val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
       ssc, kafkaParams, topicsSet)
 
     // Get the lines, split them into words, count the words and print
     val lines = messages.map(_._2)
-    val words = lines.flatMap(_.split(","))
-    //val wordCounts = words.map(x => (x, 1L)).reduceByKey(_ + _)
-    //wordCounts.print()
+    val words = lines.flatMap(_.split(" "))
+  //  val wordCounts = words.map(x => (x, 1L)).reduceByKey(_ + _)
     words.print()
 
     // Start the computation
